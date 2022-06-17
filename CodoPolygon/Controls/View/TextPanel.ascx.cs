a4088c6a975d88a7321bb7329e158761a4ad5a7c
Виктор Куликов <boom5251@inbox.ru>
2022-06-17 15:12:54 +0300

    Добавлен компонент TextPanel. Предназначен для отображения форматированного текста.

---
+++ CodoPolygon/Controls/View/TextPanel.ascx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.UI;
+
+namespace CodoPolygon.Controls.View
+{
+    public partial class TextPanel : UserControl
+    {
+        /// <summary>Содержимое текстовой панели.</summary>
+        public string Text { get; set; } = string.Empty;
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            textContent.InnerHtml = Text;
+        }
+    }
+}
